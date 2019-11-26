841b48947d80d47eba92fb0d14daa43406eb14f0
jiaxin1221 <jxhihi@163.com>
2019-11-26 09:44:45 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-home1126-2r7ypg
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-home1126-2r7ypg)
---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'HUAWEI Mate30 pro',
+    price:6899,
+    description: '麒麟990 5G旗舰SoC芯片，支持双模SA/NSA；超感光徕卡电影四摄；超曲面OLED环幕屏；40W'
+  },
+   {
+    name: 'HUAWEI Mate30',
+    price:4899,
+    description: '麒麟990 5G旗舰SoC芯片，支持双模SA/NSA；超感光徕卡电影三摄；40W有线与27W'
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: ''
+  }
+];
+
+
